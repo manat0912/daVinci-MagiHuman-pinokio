1631cba812f6bca744ffec309a25edebef9d664c
mana <manaturipa@gmail.com>
2026-03-31 21:55:47 +1300

    Delete app directory

--- app/inference/model/t5_gemma/__init__.py
+++
@@ -1,3 +0,0 @@
-from .t5_gemma_model import get_t5_gemma_embedding
-
-__all__ = ["get_t5_gemma_embedding"]
--- app/inference/model/turbo_vaed/__init__.py
+++
@@ -1,4 +0,0 @@
-from .turbo_vaed_module import TurboVAED
-from .turbo_vaed_model import get_turbo_vaed
-
-__all__ = ["TurboVAED", "get_turbo_vaed"]
